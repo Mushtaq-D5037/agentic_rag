061b97cb3a51c5ec56b2c7872e97b91adda90310
Masaa Learning <masaa.learning@gmail.com>
2025-08-31 11:02:32 +0400

    cleaned code

--- utils/embed.py
+++ utils/embed.py
@@ -18,14 +18,6 @@ def dense_embedding(text: str) -> list:
     embedding = dense_embedding_model.encode(text, normalize_embeddings=True)
     return embedding.tolist() if isinstance(embedding, np.ndarray) else embedding
     
-    # From fastembed
-    # dense_embedding_model = TextEmbedding("sentence-transformers/all-MiniLM-L6-v2")
-    # dense_embeddings = list(dense_embedding_model.passage_embed(dataset["text"][0:1]))
-    # len(dense_embeddings)
-    # print(len(dense_embeddings[0]))
-
-    # return dense_embeddings
-
 def sparse_embedding(text:str) -> list:
     
     bm25_embeddings = list(bm25_embedding_model.passage_embed(text))
@@ -35,13 +27,3 @@ def late_interaction_embedding(text:str) -> list:
     colbert_embeddings = list(colbert_embedding_model.passage_embed(text))
 
     return colbert_embeddings
-
-
-# # Testing 
-# text = 'Article 34 - Employee Wage: Work permits from the Ministry are mandatory for both employers and employees'
-# dense_embedding = dense_embedding(text)
-# sparse_embedding = sparse_embedding(text)
-# late_embedding = late_interaction_embedding(text)
-# print(len(dense_embedding))
-# print(sparse_embedding[0].as_object())
-# print(len(late_embedding[0]))
